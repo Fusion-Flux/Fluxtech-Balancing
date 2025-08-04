706249627b75bf4423ce6eeef872edad5a24abb4
Fusion-Flux <c4gamer2.0@gmail.com>
2025-08-04 11:47:46 -0500

    baby zombie fix

--- src/main/java/com/fusionflux/fluxtechbalancing/mixin/minecraft/ManualAttackGoalMixin.java
+++ src/main/java/com/fusionflux/fluxtechbalancing/mixin/minecraft/ManualAttackGoalMixin.java
@@ -3,6 +3,7 @@ package com.fusionflux.fluxtechbalancing.mixin.minecraft;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.ai.goal.ZombieAttackGoal;
+import net.minecraft.world.entity.monster.Zombie;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -21,7 +22,8 @@ public class ManualAttackGoalMixin {
             ordinal = 0
     )
     private double speedModification(double speed) {
-        if (mob)
+        if (mob instanceof Zombie && mob.isBaby())
+            return speed *1.15;
         return speed *1.25;
     }
 
